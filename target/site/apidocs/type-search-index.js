f220312100f9f6fc46414bcf7d36de8527fb6cd1
ISSG2004 <ismaelsangon1234@gmail.com>
2025-03-11 12:23:54 +0100

    Cambios

---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.damdi.ismaelsg.adressappmavenjavefx","l":"LaunchMain"},{"p":"es.damdi.ismaelsg.adressappmavenjavefx","l":"MainApp"}];updateSearchResults();
